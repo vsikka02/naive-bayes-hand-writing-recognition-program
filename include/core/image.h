4f5c4893692cc75e4ae2f767c408199c70d8f662
vanshsikka <vsikka2@illinois.edu>
2021-04-06 22:20:40 -0500

    Created an Image object and edited the data_processing_engine to incorporate the image class and create an organized data structure.

---
+++ include/core/image.h
@@ -0,0 +1,35 @@
+//
+// Created by Vansh Sikka on 4/1/21.
+//
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using std::string;
+using std::vector;
+namespace naivebayes {
+
+class Image {
+ public:
+  // Constructs a single image object which holds a vector of strings and contains
+  // the corresponding class label.
+  Image(const vector<string>& image_vector, size_t class_label);
+
+  // Overrode the output operator to print the string vector as it would
+  // appear in the training data file.
+  friend std::ostream& operator << (std::ostream& os, const Image& image);
+
+  //Getter for the image_string_vector.
+  vector<string>& image_string_vector();
+
+  //Getter for the image_class_label.
+  size_t image_class_label();
+
+ private:
+  size_t image_class_label_;
+  vector<string> image_string_vector_;
+};
+}
---
+++ src/core/image.cc
@@ -0,0 +1,29 @@
+//
+// Created by Vansh Sikka on 4/1/21.
+//
+
+#include "core/image.h"
+namespace naivebayes {
+
+Image::Image(const vector<string>& image_vector, size_t class_label) {
+  image_string_vector_ = image_vector;
+  image_class_label_ = class_label;
+}
+
+std::ostream& operator<<(std::ostream& os, const Image& image) {
+
+  for (std::string value : image.image_string_vector_) {
+    os << value << "\n";
+  }
+
+  return os;
+}
+
+std::vector<string>& Image::image_string_vector() {
+  return image_string_vector_;
+}
+
+size_t Image::image_class_label() {
+  return image_class_label_;
+}
+}  // namespace naivebayes
