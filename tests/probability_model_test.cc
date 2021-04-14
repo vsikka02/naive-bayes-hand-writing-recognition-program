34be2eb00acf6d774a868ab5ba6f3eaa5bc737b3
vanshsikka <vsikka2@illinois.edu>
2021-04-13 20:07:38 -0500

    Added Tests for Classifier.

--- tests/probability_model_test.cc
+++ tests/probability_model_test.cc
@@ -11,10 +11,9 @@
 TEST_CASE("Test Probability Model Constructor") {
   std::ifstream input_file(
       "/Users/vanshsikka/Documents/CS126/Cinder/my_projects/"
-      "naive-bayes-vsikka2/data/testtrainingimagesandlabels.txt");
+      "naive-bayes-vsikka2/tests/data/testtrainingimagesandlabels.txt");
 
   naivebayes::DataProcessingEngine test_data_engine = naivebayes::DataProcessingEngine();
-
   input_file >> test_data_engine;
 
   naivebayes::ProbabilityModel probability_model = naivebayes::ProbabilityModel(test_data_engine.image_map());
@@ -27,45 +26,42 @@ TEST_CASE("Test Probability Model Constructor") {
   }
 
   SECTION("Check Calculations in Class 1 Shaded (2 Images)") {
-
     std::vector<std::vector<float>> shaded_probability_model_1{
-        {-1.386294, -1.386294, -1.386294, -1.386294, -1.386294},
-        {-1.386294, -0.69314, -0.287682, -1.386294, -1.386294},
-        {-1.386294, -1.386294, -0.287682, -1.386294, -1.386294},
-        {-1.386294, -0.69314, -0.287682, -0.69314, -1.386294},
-        {-1.386294, -1.386294, -1.386294, -1.386294, -1.386294}};
+        {1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 2.0f/4.0f, 3.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 1.0f/4.0f, 3.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 2.0f/4.0f, 3.0f/4.0f, 2.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f}};
 
     for (size_t i = 0; i < shaded_probability_model_1.size(); i++) {
       for (size_t j = 0; j < shaded_probability_model_1.size(); j++) {
         REQUIRE(
-            shaded_probability_model_1[i][j] ==
+            log(shaded_probability_model_1[i][j]) ==
             Approx(probability_model.pixel_probability_model()[0][1][i][j]));
       }
     }
   }
 
   SECTION("Check Calculations in Class 0 Unshaded (1 Image)") {
-
     std::vector<std::vector<float>> unshaded_probability_model_0{
-        {-0.405465, -0.405465, -0.405465, -0.405465, -0.405465},
-        {-0.405465, -1.098612, -1.098612, -1.098612, -0.405465},
-        {-0.405465, -1.098612, -0.405465, -1.098612, -0.405465},
-        {-0.405465, -1.098612, -1.098612, -1.098612, -0.405465},
-        {-0.405465, -0.405465, -0.405465, -0.405465, -0.405465}};
+        {2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f}};
 
     for (size_t i = 0; i < unshaded_probability_model_0.size(); i++) {
       for (size_t j = 0; j < unshaded_probability_model_0.size(); j++) {
         REQUIRE(
-            unshaded_probability_model_0[i][j] ==
+            log(unshaded_probability_model_0[i][j]) ==
             Approx(probability_model.pixel_probability_model()[1][0][i][j]));
       }
     }
   }
   SECTION("Check Class Probabilities") {
-
     std::map<size_t, float> class_probabilities;
-    class_probabilities[0] = -0.916291;
-    class_probabilities[1] = -0.510825;
+    class_probabilities[0] = log((1.0f + 1.0f) / (2.0f + 3.0f));
+    class_probabilities[1] = log ((1.0f + 2.0f) / (2.0f + 3.0f));
 
     REQUIRE(probability_model.class_probability_model()[0] ==
             Approx(class_probabilities[0]));
@@ -77,31 +73,30 @@ TEST_CASE("Test Probability Model Constructor") {
 TEST_CASE("Read and Write JSON File", "Overridden << Operator") {
   std::ifstream input_file(
       "/Users/vanshsikka/Documents/CS126/Cinder/my_projects/"
-      "naive-bayes-vsikka2/data/testtrainingimagesandlabels.txt");
+      "naive-bayes-vsikka2/tests/data/testtrainingimagesandlabels.txt");
 
   naivebayes::DataProcessingEngine test_data_engine = naivebayes::DataProcessingEngine();
-
   input_file >> test_data_engine;
 
   naivebayes::ProbabilityModel probability_model = naivebayes::ProbabilityModel(test_data_engine.image_map());
 
   SECTION("Write JSON file") {
-    probability_model.WriteJsonOutputFile("/Users/vanshsikka/Documents/CS126/Cinder/my_projects/"
-                                            "naive-bayes-vsikka2/data/test_output_probability_model.json");
+    probability_model.WriteJsonOutputFile("/Users/vanshsikka/Documents/"
+        "CS126/Cinder/my_projects/naive-bayes-vsikka2/tests/data/test_output_probability_model.json");
   }
 
   naivebayes::ProbabilityModel new_model = naivebayes::ProbabilityModel();
 
   std::ifstream json_file(
-      "/Users/vanshsikka/Documents/CS126/Cinder/my_projects/"
-      "naive-bayes-vsikka2/data/test_output_probability_model.json");
+      "/Users/vanshsikka/Documents/CS126/Cinder/my_projects/naive-bayes-vsikka2/"
+      "tests/data/test_output_probability_model.json");
 
   json_file >> new_model;
 
   SECTION("Check Correct Class Probability Model") {
     std::map<size_t, float> class_probabilities;
-    class_probabilities[0] = -0.916291;
-    class_probabilities[1] = -0.510825;
+    class_probabilities[0] = log((1.0f + 1.0f) / (2.0f + 3.0f));
+    class_probabilities[1] = log ((1.0f + 2.0f) / (2.0f + 3.0f));
 
     REQUIRE(new_model.class_probability_model()[0] ==
             Approx(class_probabilities[0]));
@@ -112,34 +107,58 @@ TEST_CASE("Read and Write JSON File", "Overridden << Operator") {
   SECTION("Check if Probability Model 0 Unshaded is Correct") {
 
     std::vector<std::vector<float>> unshaded_probability_model_0{
-        {-0.405465, -0.405465, -0.405465, -0.405465, -0.405465},
-        {-0.405465, -1.098612, -1.098612, -1.098612, -0.405465},
-        {-0.405465, -1.098612, -0.405465, -1.098612, -0.405465},
-        {-0.405465, -1.098612, -1.098612, -1.098612, -0.405465},
-        {-0.405465, -0.405465, -0.405465, -0.405465, -0.405465}};
+        {2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f},
+        {2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f, 2.0f/3.0f}};
 
     for (size_t i = 0; i < unshaded_probability_model_0.size(); i++) {
       for (size_t j = 0; j < unshaded_probability_model_0.size(); j++) {
-        REQUIRE(unshaded_probability_model_0[i][j] ==
+        REQUIRE(log(unshaded_probability_model_0[i][j]) ==
                 Approx(new_model.pixel_probability_model()[1][0][i][j]));
       }
     }
+
   }
 
   SECTION("Check if Probability Model 1 Shaded is Correct") {
-
     std::vector<std::vector<float>> shaded_probability_model_1{
-        {-1.386294, -1.386294, -1.386294, -1.386294, -1.386294},
-        {-1.386294, -0.69314, -0.287682, -1.386294, -1.386294},
-        {-1.386294, -1.386294, -0.287682, -1.386294, -1.386294},
-        {-1.386294, -0.69314, -0.287682, -0.69314, -1.386294},
-        {-1.386294, -1.386294, -1.386294, -1.386294, -1.386294}};
+        {1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 2.0f/4.0f, 3.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 1.0f/4.0f, 3.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 2.0f/4.0f, 3.0f/4.0f, 2.0f/4.0f, 1.0f/4.0f},
+        {1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f, 1.0f/4.0f}};
 
     for (size_t i = 0; i < shaded_probability_model_1.size(); i++) {
       for (size_t j = 0; j < shaded_probability_model_1.size(); j++) {
-        REQUIRE(shaded_probability_model_1[i][j] ==
+        REQUIRE(log(shaded_probability_model_1[i][j]) ==
                 Approx(new_model.pixel_probability_model()[0][1][i][j]));
       }
     }
+
   }
+}
+
+TEST_CASE("Classifier") {
+  std::ifstream test_file ("/Users/vanshsikka/Documents/CS126/Cinder/my_projects/"
+      "naive-bayes-vsikka2/tests/data/testtestimagesandlabels.txt");
+
+  naivebayes::DataProcessingEngine data_processing = naivebayes::DataProcessingEngine();
+  test_file >> data_processing;
+
+  naivebayes::ProbabilityModel probability_model ("/Users/vanshsikka/Documents/CS126/Cinder/my_projects/"
+      "naive-bayes-vsikka2/tests/data/test_output_probability_model.json");
+
+  REQUIRE(probability_model.Classifier(data_processing.image_map()[0][0]) == 0);
+  REQUIRE(probability_model.Classifier(data_processing.image_map()[1][0]) == 1);
+}
+
+TEST_CASE("Accuracy Test") {
+  naivebayes::ProbabilityModel probability_model ("/Users/vanshsikka/Documents/"
+      "CS126/Cinder/my_projects/naive-bayes-vsikka2/data/output_probability_model.json");
+
+  REQUIRE(
+      probability_model.AccuracyOfClassifier("/Users/vanshsikka/Documents/CS126/Cinder/"
+                                             "my_projects/naive-bayes-vsikka2/data/testimagesandlabels.txt") >= 0.7);
 }
